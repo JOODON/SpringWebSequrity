d188f4c34f70567bd70ca26549d6019aef0e3910
주동호 <100278301+JOODON@users.noreply.github.com>
2022-10-10 22:45:07 +0900

    데이터베이스 까지 연결 이제 Dto만드는데 테이블 다시만들까 고민중!

--- src/main/java/config/ApplicationConfig.java
+++ src/main/java/config/ApplicationConfig.java
@@ -1,11 +1,44 @@
 package config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"dao","service"})
-public class ApplicationConfig {
+@EnableTransactionManagement
+public class ApplicationConfig implements TransactionManagementConfigurer {
 
+    private String driverClassName = "com.mysql.cj.jdbc.Driver";
+
+    private String url="jdbc:mysql://localhost:3307/MBTIDB";
+    private String username="root";
+    private String password="kkjjss103@";
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager(){
+        return transactionManager();
+    }
 }
